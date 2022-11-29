1d5fb5464ef6f80566712c712b961bd034b92e8c
JokerMixa <JokerMixa@gmail.com>
2022-11-29 10:43:20 +0300

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,15 +1,13 @@
-public class HelloWorld {
-    public static void main(String [] args) {
+import java.util.Scanner;
+
+public class Hello {
+    
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
-    }
-    int main() {
-        int n1, n2; // создаем две переменной типа int
-        cin >> n1 >> n2; // считываем данные с клавиатуры
-
-        int res = n1 * n2; // вычисляем произведение
-
-        cout << "Result: " << res << endl; // выводим результат
-
-        return 0; // завершаем работу программы
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input name: ");
+        String name = in.nextLine();
+        System.out.printf("Name: %s", name);
+        in.close();
     }
 }
